759adc004d02772010f569b0d43a35c71f426150
roni <roni.217@gmail.com>
2020-06-04 01:24:53 -0300

    add bodyparse json

--- config/customExpress.js
+++ config/customExpress.js
@@ -7,6 +7,7 @@ module.exports = () => {
     const app = express()
 
     app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
         
     consign()
         .include('controllers')
