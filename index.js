0188b31da02c5b1d252a339d705f5d13fc622ad0
roni <roni.217@gmail.com>
2020-06-05 12:02:49 -0300

    config conexao mysql

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infra/conexao')
 
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
+    }
+
+})
 const app = customExpress()
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
