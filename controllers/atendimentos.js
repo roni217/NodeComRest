6d87e59359bf4e3670c56a9cb63a7969ad709e43
roni <roni.217@gmail.com>
2020-06-07 18:19:54 -0300

    adicionando GET

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,14 +1,19 @@
 const Atendimento = require ('../models/atendimentos');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => 
-    res.send('voce esta na rota de atendimento e esta realizando um GET'));
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+    
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
 
+        Atendimento.buscaPorId(id, res)
+    })
+    
     app.post('/atendimentos', (req, res) =>{ 
-        
         const atendimento = req.body
-
         Atendimento.adiciona(atendimento, res)
-        
     });
+
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -16,6 +16,31 @@ class Atendimento {
             }
         })
     }
+
+    lista(res) {
+        const sql = 'SELECT * FROM atendimento'
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM atendimento WHERE id=${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
 }
 
 module.exports = new Atendimento
