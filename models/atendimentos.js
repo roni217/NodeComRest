7b3e626123a1afb3be97dcd16d3c509438281b29
roni <roni.217@gmail.com>
2020-06-07 22:09:18 -0300

    adicionado validacao

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,19 +2,46 @@ const moment = require('moment')
 const conexao = require('../infra/conexao')
 
 class Atendimento {
+  
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO atendimento SET ?'
-        //ou conexao.query(`INSERT INTO Servicos(nome, preco) VALUES('${nome}', '${preco}')`, (erro, resultados) => {...})}}
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro){
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    //ou conexao.query(`INSERT INTO Servicos(nome, preco) VALUES('${nome}', '${preco}')`, (erro, resultados) => {...})}}
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(atendimento)
+                }
+            })
+        }
+        
     }
 
     lista(res) {
